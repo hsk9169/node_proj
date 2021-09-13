caf3dbec158064872d125f55a6a60b99350705fa
hsk9169 <hsk9169@gmail.com>
2021-09-13 12:59:32 +0900

    trivial

--- bobjari/app.js
+++ bobjari/app.js
@@ -50,7 +50,7 @@ app.use('/', require('./routes'));
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.get('/', (req,res,next) => {
+app.get('/login', (req,res,next) => {
     res.render('./index');
 });
 
