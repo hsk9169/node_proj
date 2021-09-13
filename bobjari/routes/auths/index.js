fe78e7f4d2761e93059929e9a2d7477e82c4e347
hsk9169 <hsk9169@gmail.com>
2021-09-13 12:51:55 +0900

    add index file to route

--- bobjari/routes/auths/index.js
+++ bobjari/routes/auths/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.use('/kakao', require('/kakao'));
-router.use('/google', require('/google'));
-router.use('/apple', require('/apple'));
+router.use('/kakao', require('./kakao'));
+router.use('/google', require('./google'));
+router.use('/apple', require('./apple'));
 
 module.exports = router;
