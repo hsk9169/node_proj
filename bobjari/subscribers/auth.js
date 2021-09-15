21ca6ed47270243ed953d64c6556ce55ec897700
hsk9169 <hsk9169@gmail.com>
2021-09-15 21:54:13 +0900

    restructured kakao login

--- bobjari/subscribers/auth.js
+++ bobjari/subscribers/auth.js
@@ -26,69 +26,65 @@ class kakaoLogin {
     }
 
     getAccessToken(accessCode) {
-        const promise = new Promise((resolve, reject) => {
-            const options = {
-                method: 'POST',
-                headers: { 'content-type': 'application/x-www-form-urlencoded' },
-                data: qs.stringify({
-                    grant_type: 'authorization_code',
-                    client_id: this.clientID,
-                    redirect_uri: this.callbackURI,
-                    code: accessCode, 
-                    client_secret: this.clientSecret,
-                }),
-                url: 'https://kauth.kakao.com/oauth/token',
-            };
-    
-            axios(options)
-                .then(res => {
-                    if(res.status == 200) {
-                        const getToken = {
-                            tokenType: res.data.token_type,
-                            accessToken: res.data.access_token,
-                            expiresIn: res.data.expires_in,
-                            refreshToken: res.data.refresh_token,
-                            refreshtokenExpiresIn: res.data.refresh_token_expires_in,
-                            scope: res.data.scope,
-                        };
-                        resolve(getToken);
-                    }
-                })
-                .catch(err => {
-                    //logger.error(err);
-                    reject(err);
-                });
-        });
+        const options = {
+            method: 'POST',
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            data: qs.stringify({
+                grant_type: 'authorization_code',
+                client_id: this.clientID,
+                redirect_uri: this.callbackURI,
+                code: accessCode, 
+                client_secret: this.clientSecret,
+            }),
+            url: 'https://kauth.kakao.com/oauth/token',
+        };
+
+        axios(options)
+            .then(res => {
+                if(res.status == 200) {
+                    const getToken = {
+                        tokenType: res.data.token_type,
+                        accessToken: res.data.access_token,
+                        expiresIn: res.data.expires_in,
+                        refreshToken: res.data.refresh_token,
+                        refreshtokenExpiresIn: res.data.refresh_token_expires_in,
+                        scope: res.data.scope,
+                    };
+                    resolve(getToken);
+                }
+            })
+            .catch(err => {
+                //logger.error(err);
+                reject(err);
+            });
     }
 
     getProfile() {
-        const promise = new Promise((resolve, reject) => {
-            const options = {
-                method: 'GET',
-                headers: { 'Authorization': `Bearer ${this.accessToken}` },
-                url: 'https://kapi.kakao.com/v2/user/me',
-            };
-    
-            axios(options)
-                .then(res => {
-                    if(res.status == 200) {
-                        const getProfile = {
-                            id: res.data.id,
-                            connectedAt: res.data.connected_at,
-                            profileImg: res.data.properties.profile_image,
-                            email: res.data.kakao_account.email,
-                            ageRange: res.data.kakao_account.age_range,
-                            birthday: res.data.kakao_account.birthday,
-                            gender: res.data.kakao_account.gender,
-                        };
-                        resolve(getProfile);
-                    }
-                })
-                .catch(err => {
-                    //logger.error(err);
-                    reject(err);
-                });
-        });
+        const options = {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${this.accessToken}` },
+            url: 'https://kapi.kakao.com/v2/user/me',
+        };
+
+        axios(options)
+            .then(res => {
+                if(res.status == 200) {
+                    const getProfile = {
+                        id: res.data.id,
+                        connectedAt: res.data.connected_at,
+                        profileImg: res.data.properties.profile_image,
+                        email: res.data.kakao_account.email,
+                        ageRange: res.data.kakao_account.age_range,
+                        birthday: res.data.kakao_account.birthday,
+                        gender: res.data.kakao_account.gender,
+                    };
+                    resolve(getProfile);
+                }
+            })
+            .catch(err => {
+                //logger.error(err);
+                reject(err);
+            });
     }
 }
 
