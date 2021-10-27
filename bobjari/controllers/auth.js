5fe77acf44a7ee175cde2e0e31b6f81e84c4ae05
Hans <hans@Hans-Mac.local>
2021-10-27 12:10:21 +0900

    edit kakao signup process

--- bobjari/controllers/auth.js
+++ bobjari/controllers/auth.js
@@ -21,6 +21,7 @@ exports.authKakao = async (req, res, next) => {
 
 exports.authToken = async (req, res, next) => {
     logger.info('POST /api/auths/token');
+    console.log(req.query);
     await authService.authToken(req.query)
         .then((token) => {
             res.json(token);
