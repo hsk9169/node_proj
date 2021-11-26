d54f5357058d6598733191f982da8d13ab3b8e72
Hans <hans@Hans-Mac.local>
2021-11-26 14:00:22 +0900

    trivial

--- bobjari/api/auths/index.js
+++ bobjari/api/auths/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const authController = require('../../controllers/auth');
 
-router.post('/kakao', authController.authKakao);
-
 router.post('/email', authController.authEmail);
 
 router.get('/token', authController.authToken);
